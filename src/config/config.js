dbca731b65720ac1bee22bfe0f09a16817666777
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-07-14 16:54:36 +0530

    14072026 sales posting completed v1

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   SERVER_URL: "https://webthaali-api.onrender.com",
 
-   //SERVER_URL: "http://localhost:5000",
+  // SERVER_URL: "http://localhost:5000",
 
     CONNECTOR_VERSION: "1.0.0",
 
